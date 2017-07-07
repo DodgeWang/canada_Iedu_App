fbad0e34222120fdcd2a8dbd72b3b0bd9b7a376e
WangDaiQiang <453831794@qq.com>
2017-07-07 17:46:59 +0800

    ..

--- js/controller/home.js
+++ js/controller/home.js
@@ -33,7 +33,7 @@ function pulldownRefresh() {
 //获取首页活动
 function getActivity() {
 	XHRHTTPFunc.indexActivity(function(obj) {
-		//		console.log("thisData",JSON.stringify(obj));
+		console.log("thisData", JSON.stringify(obj));
 		if(obj.status.code !== 0) {
 			mui('#pullrefresh').pullRefresh().endPulldownToRefresh(); //refresh completed
 			mui.alert(obj.status.msg, '提示', '确定', function() {
@@ -51,22 +51,24 @@ function getActivity() {
 			var dataList = obj.data;
 			var innerDom = '';
 			for(var i = 0; i < dataList.length; i++) {
-				var itemDom = "";
-				for(var s = 0; s < dataList[i].list.length; s++) {
-					itemDom += '<li class="mui-table-view-cell mui-media mui-col-xs-6" data-src="' + dataList[i].list[s].url + '">\
+				if(dataList[i].list.length>0) {
+					var itemDom = "";
+					for(var s = 0; s < dataList[i].list.length; s++) {
+						itemDom += '<li class="mui-table-view-cell mui-media mui-col-xs-6" data-src="' + dataList[i].list[s].url + '">\
 									<a href="#"><img class="mui-media-object" src="' + dataList[i].list[s].imgpath + '">\
 										<div class="mui-media-body">' + dataList[i].list[s].title + '</div>\
 									</a>\
 								</li>'
-				}
-				innerDom += '<div class="activity-list-box">\
+					}
+					innerDom += '<div class="activity-list-box">\
 							    <div class="activity-list-title">' + dataList[i].name + '</div>\
 							    <ul class="mui-table-view mui-grid-view">' + itemDom + '</ul>\
 						    </div>'
+				}
 			}
 			document.getElementById('activity').innerHTML = innerDom;
 		} else {
-			console.log('该学生成绩暂时为空')
+			console.log('活动为空')
 		}
 		setTimeout(function() {
 			errorBox.hide();
